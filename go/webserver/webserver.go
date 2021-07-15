463066c786c65c0a8c326d9acbecc82be9725c25
Sean Liao <seankhliao@gmail.com>
2021-07-15 22:52:44 +0200

    terraform

--- go/webserver/webserver.go
+++ go/webserver/webserver.go
@@ -16,7 +16,7 @@ import (
 	"github.com/go-logr/logr"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel"
-	"go.seankhliao.com/mono/go/internal/stdlog"
+	"go.seankhliao.com/mono/go/webserver/stdlog"
 	"k8s.io/klog/v2/klogr"
 )
 
