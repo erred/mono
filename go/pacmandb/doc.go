86a9bc216adc8a9114c4e950ede2680346796c6b
Sean Liao <seankhliao@gmail.com>
2021-07-17 11:24:41 +0200

    docs

--- go/pacmandb/doc.go
+++ go/pacmandb/doc.go
@@ -3,4 +3,6 @@
 //
 // repo-add and repo-remove can be trivially implemented by
 // writing to / deleting from the map of packages in DB.
+//
+// notes: https://pkgbuild.com/~eschwartz/pacman-docs/pkg.tar.5.html
 package pacmandb
