aef1b61d3f689e04c394190176fafb87808370c2
Sean Liao <sean+git@liao.dev>
2022-04-03 23:31:05 +0100

    separate conn

--- internal/svc/otel.go
+++ internal/svc/otel.go
@@ -62,7 +62,8 @@ func newOtelClient(otlpURL string, log zerolog.Logger) (*otelclient, error) {
 		return nil, fmt.Errorf("setup otlp grpc conn to %s: %w", u.Host, err)
 	}
 
-	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
+	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithEndpoint(u.Host), otlptracegrpc.WithInsecure())
+	// traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
 	// traceExporter, err := stdouttrace.New()
 	if err != nil {
 		return nil, fmt.Errorf("setup trace exporter: %w", err)
